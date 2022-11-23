275a1d0be5ce7dfdee063c69aafca1ce4f6b66c8
Lleyton Gray <lleyton@fyralabs.com>
2022-11-22 21:54:17 -0800

    Upgrade to Next 13

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import "@fontsource/inter/variable.css";
 import { ThemeProvider } from "next-themes";
 import { NextUIProvider, createTheme } from "@nextui-org/react";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { Inter } from "@next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 const fonts = {
   sans: 'InterVariable, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
@@ -25,6 +27,11 @@ const darkTheme = createTheme({
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
       <ThemeProvider
         defaultTheme="system"
         attribute="class"
