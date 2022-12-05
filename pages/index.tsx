5fabca38428235e5a89d5415800a62664c82479b
Lleyton Gray <lleyton@fyralabs.com>
2022-12-04 19:56:57 -0800

    Get ready to PR

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import {
   Container,
   Text,
-  styled,
   Button,
   Spacer,
   Grid,
@@ -10,25 +9,8 @@ import {
 } from "@nextui-org/react";
 import Head from "next/head";
 import MainNavbar from "../components/MainNavbar";
-import Image from "next/image";
 import InstallModal from "../components/InstallModal";
 import { useState } from "react";
-
-const Divider = styled("hr", {
-  width: "100%",
-  height: "1px",
-  border: "none",
-  backgroundColor: "$gray200",
-  margin: "0",
-});
-
-const LogoImage = styled(Image, {
-  opacity: 0.5,
-  "&:hover": {
-    opacity: 1,
-  },
-  transition: "opacity 0.2s ease-in-out",
-});
 
 export default function Home() {
   const [showInstallModal, setShowInstallModal] = useState(false);
@@ -74,7 +56,8 @@ export default function Home() {
               }}
               span
             >
-              5,302
+              {/* TODO: Pull the package count in realtime */}
+              103
             </Text>{" "}
             packages that Fedora won&rsquo;t ship.
           </Text>
@@ -214,117 +197,6 @@ export default function Home() {
             </Card>
           </Grid>
         </Grid.Container>
-        {/* <Row
-          css={{
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          <Image src={terraLogo} alt="Terra Logo" width={150} />
-
-          <Text
-            css={{
-              lineHeight: "$xs",
-            }}
-            size="$8xl"
-            weight="hairline"
-          >
-            Terra
-          </Text>
-        </Row>
-
-        <Divider />
-
-        <Row
-          css={{
-            textAlign: "center",
-            mw: "750px",
-            "@smMax": {
-              textAlign: "initial",
-            },
-          }}
-        >
-          <Col
-            css={{
-              display: "flex",
-              flexDirection: "column",
-              gap: "$9",
-            }}
-          >
-            <Text
-              h1
-              css={{
-                lineHeight: "$sm",
-                margin: 0,
-                "@smMax": {
-                  fontSize: "$4xl",
-                },
-              }}
-            >
-              A modern repository for the
-              <br /> packages Fedora doesn&apos;t ship.
-            </Text>
-
-            <Text css={{ textAlign: "center" }}>
-              Finally, a repository for all the software you need. With Terra,
-              you can install the latest packages knowing that quality and
-              security are assured. Terra is also a great way to easily
-              distribute your software to users of Fedora and RPM-based
-              distributions.
-            </Text>
-
-            <Snippet
-              css={{
-                "@sm": {
-                  mx: "auto",
-                },
-                width: "min-content",
-                bg: "$accents0",
-                px: "$5",
-                py: "$3",
-              }}
-            >
-              sudo dnf config-manager --add-repo{" "}
-              <Link
-                css={{ display: "inline", color: "$primary" }}
-                href="htthttps://terra.fyralabs.com/terra.repo"
-              >
-                https://terra.fyralabs.com/terra.repo
-              </Link>
-            </Snippet>
-            <Text color="warning" small>
-              Practice caution before running any commands on your system.
-            </Text>
-
-            <Button
-              css={{
-                width: "100%",
-                maxW: 300,
-                mx: "auto",
-              }}
-              as="a"
-              href="https://github.com/terrapkg/packages"
-            >
-              Contribute
-            </Button>
-          </Col>
-        </Row>
-
-        <Divider />
-
-        <Row
-          css={{
-            justifyContent: "center",
-            gap: "$10",
-          }}
-        >
-          <a href="https://fyralabs.com">
-            <LogoImage alt="Fyra Labs Logo" src={Fyra} width={128} />
-          </a>
-          <a href="https://ultramarine-linux.org/">
-            <LogoImage alt="Ultramarine Linux Logo" src={UM} width={128} />
-          </a>
-        </Row> */}
       </Container>
     </>
   );
